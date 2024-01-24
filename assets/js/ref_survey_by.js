cc5877223306d9bcddddaaef583fdfb07a324b38
koobonyong <alex9001@codepartners.co.kr>
2024-01-24 18:06:04 +0900

    구본용 : 도시별 이미지, 3D 이미지, 기초자료 조사 업데이트

--- assets/js/ref_survey_by.js
+++ assets/js/ref_survey_by.js
@@ -1,8 +1,74 @@
 function tabMove() {
-    const mapLists = document.querySelectorAll('.map-area svg polygon');
-    let currentSigungu = '';
+    const mapLists = document.querySelectorAll('#map-country-svg polygon');
+    const mapPdf = document.querySelector('.map-pdf');
+    const pdfViewer = mapPdf.querySelector('.pdf-viewer');
+    const iframe = document.querySelector('.pdf-viewer iframe');
 
-    mapLists.forEach((mapList, index) => {
+    // iframe으로 포커스 이동
+    mapLists.forEach((mapList) => {
+        // 페이지 클릭 시 iframe부모로 포커스 이동
+        mapList.addEventListener('click', (e) => {
+            mapPdf.classList.add('on');
+            mapLists.forEach((element) => {
+                element.classList.remove('on');
+                element.setAttribute('title', '');
+            });
+            mapList.classList.add('on');
+            mapList.setAttribute('title', '선택됨');
+            // pdf 파일 로드된 후에 포커스가 들어가야 화면상 이동이 됨(추후 시간 조절 필요)
+            setTimeout(() => {
+                pdfViewer.focus();
+            }, 300);
+        });
+        // 키보드 누를 시 iframe부모로 포커스 이동
+        mapList.addEventListener('keydown', (e) => {
+            if (e.key == 'Enter' || e.key == ' ') {
+                mapPdf.classList.add('on');
+                mapLists.forEach((element) => {
+                    element.classList.remove('on');
+                    element.setAttribute('title', '');
+                });
+                mapList.classList.add('on');
+                mapList.setAttribute('title', '선택됨');
+                setTimeout(() => {
+                    pdfViewer.focus();
+                }, 300);
+            }
+        });
+    });
+
+    // iframe에서 Shift+Tab 누를 시 선택했던 페이지로 이동
+    pdfViewer.addEventListener('keydown', (e) => {
+        if (e.shiftKey && e.key == 'Tab') {
+            e.preventDefault();
+            mapLists.forEach((page) => {
+                if (page.classList.contains('on')) {
+                    setTimeout(() => {
+                        page.focus();
+                    }, 1);
+                }
+            });
+        }
+    });
+
+    const btnGoSigungu = document.querySelector('.btn-go-sigungu');
+    // 시군구 바로가기에서 Tab 누를 시 전국지도로 이동
+    btnGoSigungu.addEventListener('keydown', (e) => {
+        if (!e.shiftKey && e.key === 'Tab') {
+            e.preventDefault();
+            mapLists.forEach((page) => {
+                if (page.classList.contains('on')) {
+                    setTimeout(() => {
+                        page.focus();
+                    }, 1);
+                }
+            });
+        }
+    });
+
+    let currentSigungu = 0;
+
+    /* mapLists.forEach((mapList, index) => {
         const mapListsLength = mapLists.length - 1;
         const treeTab = document.querySelector('.comparativeTab div.on');
 
@@ -53,19 +119,10 @@ function tabMove() {
                     }, 1);
                 }
             }
-            // 마지막 도시에서 tab 할 시 공약이슈 기초조사 보기로 이동
-            // if (index == mapListsLength) {
-            //     if (e.key == 'Tab') {
-            //         e.preventDefault();
-            //         setTimeout(() => {
-            //             treeTab.parentElement.nextElementSibling.focus();
-            //         }, 1);
-            //     }
-            // }
         });
-    });
+    }); */
 
-    // 돌아가기 버튼 클릭 시 전국 지도로 돌아가기
+    // 돌아가기 버튼 이벤트 : 과장님것 붙여넣기
     const btnMapBacks = document.querySelectorAll('.btn_map_back');
 
     btnMapBacks.forEach((btnMapBack) => {
@@ -94,75 +151,43 @@ function tabMove() {
 }
 
 function pdfFile() {
-    const pdfPages = document.querySelectorAll('.pdf-cities ul li a');
-    const pdfViewer = document.querySelector('.pdf-viewer');
-
-    // iframe으로 포커스 이동
-    pdfPages.forEach((pdfPage) => {
-        // 페이지 클릭 시 iframe부모로 포커스 이동
-        pdfPage.addEventListener('click', (e) => {
-            pdfViewer.focus();
-            pdfPages.forEach((element) => {
-                element.classList.remove('on');
-                element.setAttribute('title', '');
-            });
-            pdfPage.classList.add('on');
-            pdfPage.setAttribute('title', '선택됨');
-        });
-        // 키보드 누를 시 iframe부모로 포커스 이동
-        pdfPage.addEventListener('keydown', (e) => {
-            if (e.key == 'Enter' || e.key == ' ') {
-                pdfPages.forEach((element) => {
-                    element.classList.remove('on');
-                    element.setAttribute('title', '');
-                });
-                pdfPage.classList.add('on');
-                pdfPage.setAttribute('title', '선택됨');
-                setTimeout(() => {
-                    pdfViewer.focus();
-                }, 1);
-            }
-        });
-    });
-
-    // iframe에서 Shift+Tab 누를 시 선택했던 페이지로 이동
-    pdfViewer.addEventListener('keydown', (e) => {
-        if (e.shiftKey && e.key == 'Tab') {
-            e.preventDefault();
-            pdfPages.forEach((page) => {
-                if (page.classList.contains('on')) {
-                    setTimeout(() => {
-                        page.focus();
-                    }, 1);
-                }
-            });
-        }
-    });
-
-    // iframe 내부 페이지에서 도시 선택으로 이동
-    document.querySelector('iframe').onload = function () {
-        let iframeDocument = document.querySelector('iframe').contentWindow.document;
-        // view.html viewerContainer 특정
-        let iframePage = iframeDocument.querySelector('#viewerContainer');
-        iframePage.addEventListener('keydown', function (e) {
-            if (!e.shiftKey && e.key === 'Tab') {
-                e.preventDefault();
-                pdfPages.forEach((page) => {
-                    if (page.classList.contains('on')) {
-                        setTimeout(() => {
-                            page.focus();
-                        }, 1);
-                    }
-                });
-            }
-        });
+    const mapLists = document.querySelectorAll('#map-country-svg polygon');
+    const iframe = document.querySelector('.pdf-viewer iframe');
+    // 객체에 pdf 파일 위치 모아놓고 data 비교후 적용
+    let mapPDF = {
+        서울: './assets/pdf_viewer/web/viewer.html?file=231215_정책선거문화_확산을_위한_언론기사_빅데이터_분석.pdf#page=25',
+        부산: './assets/pdf_viewer/web/viewer.html?file=231215_정책선거문화_확산을_위한_언론기사_빅데이터_분석.pdf#page=73',
+        대구: './assets/pdf_viewer/web/viewer.html?file=231215_정책선거문화_확산을_위한_언론기사_빅데이터_분석.pdf#page=65',
     };
+
+    // polygon 이벤트
+    mapLists.forEach((mapList) => {
+        //클릭 시 iframe src: pdf 파일 변경
+        mapList.addEventListener('click', () => {
+            for (let i in mapPDF) {
+                if (mapList.getAttribute('data-area') == i) {
+                    iframe.src = mapPDF[i];
+                }
+            }
+        });
+        //키보드 엔터 시 iframe src: pdf 파일 변경
+        mapList.addEventListener('keydown', (e) => {
+            if (e.key == 'Enter' || e.key == ' ') {
+                for (let i in mapPDF) {
+                    if (mapList.getAttribute('data-area') == i) {
+                        iframe.src = mapPDF[i];
+                    }
+                }
+            }
+        });
+    });
 }
 
 // 현재 위치 확인
-document.addEventListener('keydown', (e) => {
-    // console.log(document.activeElement);
-});
+/* document.addEventListener('keydown', (e) => {
+    console.log(document.activeElement);
+}); */
+
 // html 로드되고 바로 실행
 window.addEventListener('DOMContentLoaded', function () {
     tabMove();
